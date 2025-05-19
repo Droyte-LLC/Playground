b071f63c3cd405cdb3e4a5f756884c66842b0265
droyte <gregory.jean-baptiste@droyte.com>
2025-05-19 02:46:07 -0400

    initial commit

---
+++ MineSweeper/MineSweeperGame.cs
@@ -0,0 +1,14 @@
+﻿namespace MineSweeper;
+
+public class MineSweeperGame
+{
+  static char[,] board = {
+    { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
+    { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
+    { 'E', 'E', 'M', 'E', 'E', 'E', 'E', 'E' },
+    { 'E', 'E', 'M', 'E', 'E', 'E', 'E', 'E' },
+    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
+    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
+    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
+  };
+}
