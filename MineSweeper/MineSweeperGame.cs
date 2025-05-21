d88717f60022b24776f45daf9ac90cfa9379c722
droyte <gregory.jean-baptiste@droyte.com>
2025-05-20 22:26:05 -0400

    added MineSweeper fun project

--- MineSweeper/MineSweeperGame.cs
+++ MineSweeper/MineSweeperGame.cs
@@ -2,13 +2,153 @@
 
 public class MineSweeperGame
 {
-  static char[,] board = {
-    { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
-    { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
-    { 'E', 'E', 'M', 'E', 'E', 'E', 'E', 'E' },
-    { 'E', 'E', 'M', 'E', 'E', 'E', 'E', 'E' },
-    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
-    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
-    { 'E', 'E', 'M', 'M', 'M', 'M', 'M', 'M' },
-  };
+  private char[,] board;
+  private int rows;
+  private int cols;
+  private int mines;
+  private bool gameOver;
+
+  public MineSweeperGame(char[,] board)
+  {
+    this.board = board;
+    this.rows = board.GetLength(0);
+    this.cols = board.GetLength(1);
+    this.mines = GetMinesCount();
+  }
+
+  public MineSweeperGame()
+  {
+    this.board = new char[,]
+    {
+      { 'E', 'E', 'E', 'M', 'E', 'E', 'E', 'E' },
+      { 'E', 'M', 'E', 'E', 'E', 'E', 'E', 'E' },
+      { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'M' },
+      { 'M', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
+      { 'E', 'E', 'E', 'E', 'M', 'M', 'E', 'M' },
+      { 'E', 'E', 'M', 'M', 'E', 'E', 'M', 'M' },
+      { 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E' },
+      { 'E', 'E', 'E', 'E', 'M', 'E', 'E', 'E' }
+    };
+
+    this.rows = board.GetLength(0);
+    this.cols = board.GetLength(1);
+    this.mines = GetMinesCount();
+    this.gameOver = false;
+  }
+
+  private int GetMinesCount()
+  {
+    int mines = 0;
+
+    foreach (char c in board)
+    {
+      if (c == 'M')
+      {
+        mines++;
+      }
+    }
+
+    return mines;
+  }
+  public char[,] GetBoard()
+  {
+    return board;
+  }
+
+  private int[][] directions =
+  [
+    [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]
+  ];
+
+  public bool IsGameOver()
+  {
+    return gameOver;
+  }
+
+  public bool IsGameWon()
+  {
+    int clearedCells = 0;
+    foreach (char c in board)
+    {
+      if (c != 'E' && c != 'M')
+        clearedCells++;
+    }
+    return clearedCells == (rows * cols - mines);
+  }
+
+  public void UpdateBoard(int clickedRow, int ClickedCol)
+  {
+    if (board[clickedRow, ClickedCol] == 'M')
+    {
+      board[clickedRow, ClickedCol] = 'X';
+      this.gameOver = true;
+      return;
+    }
+
+    Queue<(int, int)> queue = new Queue<(int, int)>();
+    queue.Enqueue((clickedRow, ClickedCol));
+    bool[,] visited = new bool[rows, cols];
+
+    while (queue.Count > 0)
+    {
+      var (r, c) = queue.Dequeue();
+
+      if (visited[r, c])
+      {
+        continue;
+      }
+
+      visited[r, c] = true;
+
+      int mines = CountAdjacentMines(r, c);
+      if (mines > 0)
+      {
+        board[r, c] = (char)(mines + '0');
+      }
+      else
+      {
+        board[r, c] = 'B';
+        foreach (var dir in directions)
+        {
+          int nr = r + dir[0];
+          int nc = c + dir[1];
+          if (InBounds(nr, nc) && board[nr, nc] == 'E')
+            queue.Enqueue((nr, nc));
+        }
+      }
+    }
+
+    if (IsGameWon())
+    {
+      gameOver = true;
+    }
+  }
+
+  int CountAdjacentMines(int row, int col)
+  {
+    int count = 0;
+    foreach (var dir in directions)
+    {
+      int r = row + dir[0];
+      int c = col + dir[1];
+      if (InBounds(r, c) && board[r, c] == 'M')
+        count++;
+    }
+    return count;
+  }
+
+  bool InBounds(int r, int c)
+  {
+    return r >= 0 && r < rows && c >= 0 && c < cols;
+  }
+
+  public void ShowBoard()
+  {
+    for (int r = 0; r < rows; r++)
+    {
+      for (int c = 0; c < cols; c++)
+        Console.Write(board[r, c] + " ");
+      Console.WriteLine();
+    }
+  }
 }
--- PlaygroundConsole/Program.cs
+++ PlaygroundConsole/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to PlaygroundConsole, let's play Minesweeper!");
+MineSweeperGame sweeperGame = new();
+
+Console.WriteLine("\nGood luck! Try not to hit a mine...");
+
+sweeperGame.ShowBoard();
+
+while (!sweeperGame.IsGameOver())
+{
+  Console.WriteLine("Enter the row and column of the cell you want to click.  Example: 2 3");
+  string? input = Console.ReadLine();
+  string[] parts = input?.Split(' ') ?? Array.Empty<string>();
+
+  if (parts.Length != 2 || !int.TryParse(parts[0], out int row) || !int.TryParse(parts[1], out int col))
+  {
+    Console.WriteLine("Please enter two valid numbers, separated by a space.  Example: 2 3");
+    continue;
+  }
+
+  sweeperGame.UpdateBoard(row, col);
+  sweeperGame.ShowBoard();
+}
+
+if (sweeperGame.IsGameWon())
+  Console.WriteLine("Congratulations! You cleared the board without hitting a mine!");
+else
+  Console.WriteLine("Game Over! You hit a mine.  Thank you for playing!");
