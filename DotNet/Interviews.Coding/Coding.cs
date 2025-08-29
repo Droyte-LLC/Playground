afdc24f362037bc500e52e411cfaf1b44f9f9392
droyte <gregory.jean-baptiste@droyte.com>
2025-08-29 10:17:09 -0400

    matchString code

--- DotNet/Interviews.Coding/Coding.cs
+++ DotNet/Interviews.Coding/Coding.cs
@@ -10,10 +10,10 @@ public class Coding
   public static Dictionary<int, int> GetSortedOccurrenceDictionary(List<int> list)
   {
     var frequencyDict = list
-        .GroupBy(n => n)
-        .OrderBy(g => g.Count())   // Sort by count ascending
-        .ThenBy(g => g.Key)        // Then by number ascending
-        .ToDictionary(g => g.Key, g => g.Count());
+      .GroupBy(n => n)
+      .OrderBy(g => g.Count())   // Sort by count ascending
+      .ThenBy(g => g.Key)        // Then by number ascending
+      .ToDictionary(g => g.Key, g => g.Count());
 
     // Output the sorted dictionary
     foreach (var kvp in frequencyDict)
@@ -22,5 +22,58 @@ public class Coding
     }
 
     return frequencyDict;
+  }
+
+  /*
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+  */
+
+  public static List<int> matchingStrings(List<string> stringList, List<string> queries)
+  {
+    List<int> results = new();
+
+    //get frequency count
+    Dictionary<string, int> frequencyCount = new();
+
+    for (int i = 0; i < queries.Count; i++)
+    {
+      if (stringList.Contains(queries[i]))
+      {
+        if (frequencyCount.ContainsKey(queries[i]))
+        {
+          frequencyCount[queries[i]] = frequencyCount[queries[i]] + 1;
+        }
+        else
+        {
+          frequencyCount.Add(queries[i], 1);
+        }
+      }
+      else
+      {
+        frequencyCount.Add(queries[i], 0);
+      }
+    }
+
+    for (int i = 0; i < queries.Count; i++)
+    {
+      if (frequencyCount.ContainsKey(queries[i]))
+      {
+        results.Add(frequencyCount[queries[i]]);
+      }
+      else
+      {
+        results.Add(0);
+      }
+    }
+
+    return results;
   }
 }
